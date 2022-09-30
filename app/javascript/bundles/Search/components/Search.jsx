771b08891878b35b10364b7fb7734818e2d81c41
dwikov <mdwik@provectus.com>
2022-10-01 02:26:56 +0300

    fix deployment issues

--- app/javascript/bundles/Search/components/Search.jsx
+++ app/javascript/bundles/Search/components/Search.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import BarChart from './BarChart';
 import ResultsTable from './Table';
-import './Search.module.css';
 import { Container } from 'react-bootstrap';
 import { getAnalytics, matchAll, postAnalytics } from '../api';
 
@@ -40,11 +38,11 @@ const Search = () => {
 
 
   return (<>
-      <Container className='mt-5'>
+      <Container style={{marginTop: '5%'}}>
         <Form>
           <Form.Control type="search" placeholder="Search here" value={query} onChange={handleControlChange}/>
         </Form>
-        <Container className='overflow-auto' style={{ height: '350px'}}>
+        <Container style={{overflow: 'auto', height: '350px'}}>
           <ResultsTable results={results} />
         </Container>
       </Container>
